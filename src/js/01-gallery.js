68b3415f6be840001d6e39f1c4a736c98688d1f3
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-01-26 17:38:12 +0200

    almost finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const divParentGallery = document.querySelector('.gallery');
+const imageMarkup = createImageGalery(galleryItems);
+divParentGallery.innerHTML = imageMarkup;
+divParentGallery.addEventListener('click', onclickPhotoInGallery);
+function createImageGalery(galleryItems) {
+    return galleryItems.map((gallery) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${gallery.preview}"
+                    data-source="${gallery.original}"
+                    alt="${gallery.description}"
+                />
+                </a>
+            </div>`;
+        })
+        .join('');
+};
+function onclickPhotoInGallery(event) {
+    // Не перекидає на іншу сторінку
+    event.preventDefault();
+    
+    // клікає лише на фотографію 
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+    `)
+    instance.show();
+
+    divParentGallery.addEventListener('keydown', (event) => {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    });
+};
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const divParentGallery = document.querySelector('.gallery');
+const imageMarkup = createImageGalery(galleryItems);
+divParentGallery.innerHTML = imageMarkup;
+divParentGallery.addEventListener('click', onclickPhotoInGallery);
+function createImageGalery(galleryItems) {
+    return galleryItems.map((gallery) => {
+        return `
+            <a class="gallery__item" href="${gallery.original}">
+                <img class="gallery__image" src="${gallery.preview}" alt="${gallery.description}" />
+            </a>`;
+        })
+        .join('');
+};
+function onclickPhotoInGallery(event) {
+    // Не перекидає на іншу сторінку
+    event.preventDefault();
+    // slider photo
+    // клікає лише на фотографію 
+    divParentGallery.addEventListener('keydown', (event) => {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    });
+};
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+});
